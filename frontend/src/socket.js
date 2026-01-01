40659177a07cd07579d4e5fbff52533ef2bc4b85
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2026-01-01 19:08:39 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://four-in-a-row-xlwv.onrender.com");
+fetch("https://four-in-a-row-xlwv.onrender.com/api/leaderboard")
+fetch("https://four-in-a-row-xlwv.onrender.com/api/analytics")
 
 export default socket;
